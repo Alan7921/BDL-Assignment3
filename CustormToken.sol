9035bc48ff3c153d8c587e8414d330340e0d8727
Alan7921 <iamliuxin2020@gmail.com>
2021-12-24 22:05:55 +0000

    Update CustormToken.sol

--- CustormToken.sol
+++ CustormToken.sol
@@ -1,13 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./CustomLib.sol";
+
 contract token{
 
 	//a uint256 that defines the price of your token in wei; each token can be purchased with tokenPrice wei
-	uint256 public tokenPrice = 1;
+	uint256 public tokenPrice;
 	uint256 public existAmount;
 	mapping (address => uint256) balance;
 	address public owner;
 
-	constructor(){
+	constructor(uint256 initialPrice){
         owner = msg.sender; // assign the address of owner
+        tokenPrice = initialPrice;
     }
 
 	event Purchase(address buyer, uint256 amount);
@@ -20,11 +27,18 @@ contract token{
 		and emits an event Purchase with the buyer’s address and the purchased amount
 	*/
 	function buyToken(uint256 amount) public returns (bool isSuccessful) {
-		require(msg.value >= tokenPrice*amount,"Insufficient value.")
+		require(msg.value >= tokenPrice*amount,"Insufficient value, please check current price then send enough value.")
+
+		//resist overflow 
 		require (balance [msg.sender] + amount >= balance[sender],"sorry,your balance is full now.")
-		balance [msg.sender] += amount;
 		require (existAmount + amount >= existAmount,"sorry,currently the exist token approach its limitation.")
+
+		balance [msg.sender] += amount;
 		existAmount += amount;
+
+		emit Purchase(msg.sender,amount);
+
+		return true;
 	}
 
 	/*	a function that transfers amount number of
@@ -33,6 +47,16 @@ contract token{
 		sender’s and receiver’s addresses and the transferred amount
 	*/
 	function transfer(address recipient, uint256 amount) public returns (bool isSuccessful) {
+		require(balance[msg.sender] >= amount, "You do not have enough balance.");
+
+		balance[msg.sender] -= amount;
+
+		require (balance[recipient] + amount >= balance[recipient],"sorry,the recipient's balance is full now.")
+		balance[recipient] += amount;
+
+		emit Transfer(msg.sender,msg.recipient,amount);
+
+		return true;
 
 	}	
 
@@ -42,7 +66,24 @@ contract token{
 		event Sell with the seller’s address and the sold amount of tokens
 	*/
 	function sellToken(uint256 amount) public returns (bool isSuccessful) {
+		//caution: re-entrancy
 
+		require(address(this).balance >= amount*tokenPrice, "Sorry,currently we cannot provide this service.");
+		require(balance[msg.sender] >= amount, "Sorry, you do not have enough token in your balance.");
+
+		// resist re-entrancy
+		require(balance[msg.sender] - amount < balance[msg.sender]);
+		balance[msg.sender] -= amount;
+
+		//
+		require (existAmount - amount < existAmount);
+		existAmount -= amount;
+
+		//make the payment to the customer
+		customLib.customSend(amount*tokenPrice, msg.sender);
+
+		emit Sell(msg.sender, amount);
+		return true;
 	}		
 
 
@@ -52,7 +93,15 @@ contract token{
 		contract’s funds suffice so that all tokens can be sold for the updated price)
 	*/
 	function changePrice(uint256 price) public returns (bool isSuccessful) {
+		require(msg.sender == owner, "You are not authorized to change the price.");
 
+		require(address(this).balance >= existAmount * price, 
+			"The contract do not have sufficient banlance" 
+			"to pay for all the existed token according the price to be changed.");
+
+		emit Price(price);
+
+		return true;
 	}
 
 	/*
